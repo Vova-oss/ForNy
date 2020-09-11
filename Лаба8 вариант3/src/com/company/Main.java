5771923332d79cfc7895a9acb75cc0fde88db057
Vova <ogyrcka@gmail.com>
2020-09-11 11:02:11 +0300

    Второе сохранение

--- Лаба8 вариант3/src/com/company/Main.java
+++ Лаба8 вариант3/src/com/company/Main.java
@@ -8,11 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         try(FileWriter writer = new FileWriter("C:\\Users\\Saint\\Desktop\\test.txt.txt")){
-            System.out.println("Введите текст:");
-            Scanner sc = new Scanner(System.in);
-            String stroke = sc.nextLine();
-            writer.write(stroke);
-            System.out.println("Текст успешно записан в файл");
+           
 
         } catch (IOException e) {
             e.printStackTrace();
